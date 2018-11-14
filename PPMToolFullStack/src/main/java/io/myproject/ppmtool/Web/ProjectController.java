518fac14c5b9d75f5c5677f207565717b9bcfc6a
Jaimejourney <jaimejourney@163.com>
2018-11-13 23:21:18 -0800

    update project

--- PPMToolFullStack/src/main/java/io/myproject/ppmtool/Web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/myproject/ppmtool/Web/ProjectController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
     @Autowired
     private ProjectService projectService;
